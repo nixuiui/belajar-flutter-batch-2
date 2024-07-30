f603b19be2d42a3a1278ccbe9d09b1e14f277347
Niki Rahmadi Wiharto <nikirahmadiwiharto@gmail.com>
2024-07-30 21:19:08 +0700

    add contact list view

---
+++ lib/models/contact.dart
@@ -0,0 +1,11 @@
+class Contact {
+  final String name;
+  final String phoneNumber;
+  final String intial;
+
+  Contact({
+    required this.name,
+    required this.phoneNumber,
+    required this.intial,
+  });
+}
--- lib/tabs/profile_tab.dart
+++ lib/tabs/profile_tab.dart
@@ -1,3 +1,4 @@
+import 'package:belajar_flutter_batch_2/models/contact.dart';
 import 'package:flutter/material.dart';
 
 class ProfileTab extends StatefulWidget {
@@ -8,10 +9,126 @@ class ProfileTab extends StatefulWidget {
 }
 
 class _ProfileTabState extends State<ProfileTab> {
+
+  final contactList = [
+    Contact(
+      name: "John Lenon", 
+      phoneNumber: "+1823232932932", 
+      intial: "JL"
+    ),
+    Contact(
+      name: "Ibrahim", 
+      phoneNumber: "+1823322333333", 
+      intial: "I"
+    ),
+    Contact(
+      name: "Kaka", 
+      phoneNumber: "+1823329948874", 
+      intial: "K"
+    ),
+    Contact(
+      name: "John Lenon", 
+      phoneNumber: "+1823232932932", 
+      intial: "JL"
+    ),
+    Contact(
+      name: "Ibrahim", 
+      phoneNumber: "+1823322333333", 
+      intial: "I"
+    ),
+    Contact(
+      name: "Kaka", 
+      phoneNumber: "+1823329948874", 
+      intial: "K"
+    ),
+    Contact(
+      name: "John Lenon", 
+      phoneNumber: "+1823232932932", 
+      intial: "JL"
+    ),
+    Contact(
+      name: "Ibrahim", 
+      phoneNumber: "+1823322333333", 
+      intial: "I"
+    ),
+    Contact(
+      name: "Kaka", 
+      phoneNumber: "+1823329948874", 
+      intial: "K"
+    ),
+    Contact(
+      name: "John Lenon", 
+      phoneNumber: "+1823232932932", 
+      intial: "JL"
+    ),
+    Contact(
+      name: "Ibrahim", 
+      phoneNumber: "+1823322333333", 
+      intial: "I"
+    ),
+    Contact(
+      name: "Kaka", 
+      phoneNumber: "+1823329948874", 
+      intial: "K"
+    ),
+    Contact(
+      name: "John Lenon", 
+      phoneNumber: "+1823232932932", 
+      intial: "JL"
+    ),
+    Contact(
+      name: "Ibrahim", 
+      phoneNumber: "+1823322333333", 
+      intial: "I"
+    ),
+    Contact(
+      name: "Kaka", 
+      phoneNumber: "+1823329948874", 
+      intial: "K"
+    ),
+    Contact(
+      name: "John Lenon", 
+      phoneNumber: "+1823232932932", 
+      intial: "JL"
+    ),
+    Contact(
+      name: "Ibrahim", 
+      phoneNumber: "+1823322333333", 
+      intial: "I"
+    ),
+    Contact(
+      name: "Kaka", 
+      phoneNumber: "+1823329948874", 
+      intial: "K"
+    ),
+  ];
+
   @override
   Widget build(BuildContext context) {
-    return const Center(
-      child: Text('This is Profile Tab'),
+    return ListView.builder(
+      itemCount: contactList.length,
+      itemBuilder: (context, index) => ListTile(
+        leading: Container(
+          width: 60,
+          height: 60,
+          decoration: BoxDecoration(
+            color: Colors.green,
+            borderRadius: BorderRadius.circular(50)
+          ),
+          child: Center(
+            child: Text(
+              contactList[index].intial,
+              style: const TextStyle(
+                fontSize: 20,
+                color: Colors.white,
+                fontWeight: FontWeight.bold
+              ),
+            )
+          ),
+        ),
+        title: Text(contactList[index].name),
+        subtitle: Text(contactList[index].phoneNumber),
+      ), 
     );
   }
 }
